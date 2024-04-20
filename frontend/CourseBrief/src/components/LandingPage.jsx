433c99b3e316f5f30899eff29f61d87d78d89888
Ibrahim Dobashi <1br4h1m.d0@gmail.com>
2024-04-19 22:35:36 -0700

    added sign up and working on landing page

---
+++ frontend/CourseBrief/src/components/LandingPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+import { useNavigate } from 'react-router-dom';
+const LandingPage = () => {
+    const navigate = useNavigate();
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h1>Welcome to our Landing Page</h1>
+                    <p>Thank you for visiting our website!</p>
+                    <Button variant="primary" onClick={() => navigate('login')} >Get Started</Button>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default LandingPage;
---
+++ frontend/CourseBrief/src/components/SignUp.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+
+import {
+  Container,
+  Navbar,
+  Nav,
+  Button,
+  Form,
+  Row,
+  Col,
+  FormControl,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+const SignUpPage = () => {
+  const navigate = useNavigate();
+  return (
+    <section className="section-style">
+      <Container className="container-style my-5 py-5">
+        <Row className="d-flex justify-content-center">
+          <Col className="d-flex flex-column">
+            <Col
+              className="d-flex flex-fill justify-content-center align-self-center my-5"
+              style={{ height: "12%", width: "60%" }}
+            >
+              <Button
+                className="btn-primary flex-fill btn-hover button-signin"
+                type="button"
+                onClick={() => navigate("/login")}
+              >
+                Sign In
+              </Button>
+              <Button
+                className="btn-primary flex-fill btn-hover button-signup"
+                type="button"
+               
+              >
+                Sign Up
+              </Button>
+            </Col>
+            <div className="d-flex justify-content-center align-items-center align-self-center m-5 form-container">
+              <Col md={5} xl={6} className="text-center text-md-start">
+                <Form method="post" data-bs-theme="light">
+                  <Row className="mb-4">
+                    <Col xs={12} md={6}>
+                      <Form.Group>
+                        <FormControl
+                          type="text"
+                          name="FirstName"
+                          placeholder="First Name"
+                          className="shadow"
+                        />
+                      </Form.Group>
+                    </Col>
+                    <Col xs={12} md={6}>
+                      <Form.Group>
+                        <FormControl
+                          type="text"
+                          name="LastName"
+                          placeholder="Last Name"
+                          className="shadow"
+                        />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                  <Form.Group className="mb-4">
+                    <FormControl
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      className="shadow"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-4">
+                    <FormControl
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      className="shadow"
+                    />
+                  </Form.Group>
+                  <div className="d-flex justify-content-center mb-4">
+                    <Button
+                      type="submit"
+                      className="btn-primary shadow button-login"
+                    >
+                      Sign Up
+                    </Button>
+                  </div>
+                </Form>
+              </Col>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default SignUpPage;
