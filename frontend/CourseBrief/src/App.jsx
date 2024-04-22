5cf29c842797b1a4365548ea00a74292d427e661
TimothyPhan2 <98495706+TimothyPhan2@users.noreply.github.com>
2024-04-21 19:35:41 -0700

    Added Navbar

--- frontend/CourseBrief/src/App.jsx
+++ frontend/CourseBrief/src/App.jsx
@@ -1,15 +1,5 @@
 import React from "react";
 
-import {
-  Container,
-  Navbar,
-  Nav,
-  Button,
-  Form,
-  Row,
-  Col,
-  FormControl,
-} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './components/Login.jsx';
@@ -17,15 +7,19 @@ import SignUpPage from './components/SignUp.jsx';
 import LandingPage from "./components/LandingPage.jsx";
 
 import "./App.css"; 
+import NavBar from "./components/NavBar.jsx";
 
 const App = () => {
   return (
+    <>
+    <NavBar />
     <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="login"  element={<LoginPage />} />
           
           <Route path="signup" element={<SignUpPage />} />
       </Routes>
+    </>
 
     );
 };
