54e4c7a9b2a5e49a0edb81d9b03d15a79635d7da
Raul Garcia <raul.garcia01@sjsu.edu>
2024-05-08 15:27:30 -0700

    adding queries

--- backend/src/main/java/com/CS157AProject/CourseBrief/repository/CommentRepository.java
+++ backend/src/main/java/com/CS157AProject/CourseBrief/repository/CommentRepository.java
@@ -1,12 +1,13 @@
 package com.CS157AProject.CourseBrief.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.CS157AProject.CourseBrief.model.Comment;
 
 public interface CommentRepository extends JpaRepository <Comment, String>{
+    @Query("SELECT c FROM Comment c WHERE c.commentID = :commentID")
     Comment findCommentByCommentID(String commentID);
-    
-   
+
 
 }
