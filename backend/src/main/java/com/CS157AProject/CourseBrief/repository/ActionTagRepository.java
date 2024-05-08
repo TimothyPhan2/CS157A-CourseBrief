5ec0cb8ce7948495836eb290153b2f881fa914df
Raul Garcia <raul.garcia01@sjsu.edu>
2024-05-08 13:56:40 -0700

    action tag fix

--- backend/src/main/java/com/CS157AProject/CourseBrief/repository/ActionTagRepository.java
+++ backend/src/main/java/com/CS157AProject/CourseBrief/repository/ActionTagRepository.java
@@ -9,7 +9,8 @@ import com.CS157AProject.CourseBrief.model.ActionTag;
 
 @Repository
 public interface ActionTagRepository extends JpaRepository<ActionTag, String> {
-    ActionTag findActionTagByActionID(String actionID);
+    @Query("SELECT at FROM ActionTag at WHERE at.actionID = :actionID")
+    ActionTag findActionTagByActionID(@Param("actionID")String actionID);
     
     @Query("SELECT MAX(a.actionID) FROM ActionTag a")
     String findHighestActionTagId();
@@ -17,7 +18,7 @@ public interface ActionTagRepository extends JpaRepository<ActionTag, String> {
     @Query(value = "SELECT MAX(CAST(SUBSTRING(a.actionID, :startPos) AS UNSIGNED)) FROM action_tag a", nativeQuery = true)
     int findHighestActionTagId(@Param("startPos") int startPos);
 
-    @Query("SELECT at FROM ActionTag at WHERE at.tagID = :tagID")
+    @Query("SELECT at FROM ActionTag at WHERE at.tag.tagID = :tagID")
     ActionTag findActionTagByTag_TagID(@Param("tagID")String tagID);
 }
     
