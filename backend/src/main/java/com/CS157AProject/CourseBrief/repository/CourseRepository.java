bcd5c947593b60abf563c60377c286250d54969b
TimothyPhan2 <98495706+TimothyPhan2@users.noreply.github.com>
2024-05-09 04:45:35 -0700

    Update CourseRepository.java

--- backend/src/main/java/com/CS157AProject/CourseBrief/repository/CourseRepository.java
+++ backend/src/main/java/com/CS157AProject/CourseBrief/repository/CourseRepository.java
@@ -1,14 +1,16 @@
 package com.CS157AProject.CourseBrief.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import com.CS157AProject.CourseBrief.model.Course;
 
 @Repository
-public interface CourseRepository extends JpaRepository <Course, String> {
-
+public interface CourseRepository extends JpaRepository<Course, String> {
 
         Course findCourseByCourseID(String courseID);
 
@@ -56,20 +58,4 @@ public interface CourseRepository extends JpaRepository <Course, String> {
                         "HAVING COUNT(DISTINCT ct.tag.label) >= (SELECT COUNT(t) FROM Tag t WHERE t.label IN :tagLabels)")
         List<Course> findCoursesByCriteria(@Param("firstName") String firstName, @Param("lastName") String lastName,
                         @Param("className") String className, @Param("tagLabels") List<String> tagLabels);
-
-    Course findCourseByCourseID(String courseID);
-
-    @Query("SELECT c FROM Course c WHERE c.aClass.classID = :classID AND c.professor.professorID = :professorID")
-    Course findCourseByClassIDAndProfessorID(@Param("classID")String classID, @Param("professorID")String professorID);
-
-    boolean existsByCourseID(String courseID); 
-
-    /* @Query("SELECT MAX(c.courseID) FROM Course c")
-    String findHighestCourseId(); */
-
-    @Query(value = "SELECT MAX(CAST(SUBSTRING(c.courseID, :startPos) AS UNSIGNED)) FROM Course c", nativeQuery = true)
-    Integer findHighestCourseId(@Param("startPos") int startPos);
-
-    
-
 }
